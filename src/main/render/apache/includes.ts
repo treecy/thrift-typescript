49f887e779503a0572af2fd17590acb49a462c51
Tracy | Liu Cui <ts-cui.liu@rakuten.com>
2019-03-20 15:49:57 +0900

    change the int64 import clause

--- src/main/render/apache/includes.ts
+++ src/main/render/apache/includes.ts
@@ -80,11 +80,11 @@ export function renderImport(name: string, includePath: string) {
  * Creates an import for Int64 type if it is being used by the file we're
  * generating. We'll need to keep track of what each files uses.
  */
-export function renderInt64Import(): ts.ImportEqualsDeclaration {
-    return ts.createImportEqualsDeclaration(
+export function renderInt64Import() {
+    return ts.createImportDeclaration(
         undefined,
         undefined,
-        COMMON_IDENTIFIERS.Node_Int64,
-        ts.createExternalModuleReference(ts.createLiteral('node-int64')),
+        ts.createImportClause(COMMON_IDENTIFIERS.Node_Int64, undefined),
+        ts.createLiteral('node-int64'),
     )
 }
